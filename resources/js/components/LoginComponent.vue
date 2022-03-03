b933d96d6e4ae8782e1be0f6baa66dada030f1c1
Raul Lendines <raullendines@gmail.com>
2022-03-03 13:23:40 +0100

    Form

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -1,123 +1,58 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      :error-messages="selectErrors"
-      label="Item"
-      required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :error-messages="checkboxErrors"
-      label="Do you agree?"
-      required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
-    ></v-checkbox>
-
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
+    <div class="container border border-dark rounded bg-light">
+        <div class="row justify-content-center m-4">
+            <div class="col-md-4">
+                <img src="" alt="">
+            </div>
+            <div class="col-md-4">
+                <h1>Login</h1>
+                <form needs-validation>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="Adreça electrònica" oninput="validation()">
+                        <label for="email">Adreça electrònica</label>
+                        <div id="feedback-email"> </div>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="password" placeholder="Contrassenya" oninput="validation()">
+                        <label for="password">Contrassenya</label>
+                        <div id="feedback-password"></div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn rounded-pill" type="button" style="background-color: #00B0C8">Accedir</button>
+                    </div>
+                </form>
+            </div>           
+        </div>
+    </div>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email } from 'vuelidate/lib/validators'
+    function viewPassword() {
+        var password = document.getElementById("password");
+            if (password.type === "password") 
+            {
+                password.type = "text";
+            } else 
+            {
+                password.type = "password";
+            }
+    }
 
-  export default {
-    mixins: [validationMixin],
+    function validation(){
+        var password = document.getElementById("password").value;
+        var email = document.getElementById("email").value;
 
-    validations: {
-      name: { required, maxLength: maxLength(10) },
-      email: { required, email },
-      select: { required },
-      checkbox: {
-        checked (val) {
-          return val
-        },
-      },
-    },
-
-    data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
-
-    computed: {
-      checkboxErrors () {
-        const errors = []
-        if (!this.$v.checkbox.$dirty) return errors
-        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-        return errors
-      },
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-      },
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
-    },
-
-    methods: {
-      submit () {
-        this.$v.$touch()
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-      },
-    },
-  }
+        if (password.length >= 10) {
+            var mensaje = "Correcto";
+        }else{
+            var mensaje = "Error";
+        }
+        
+        document.getElementById("feedback-password").innerHTML = mensaje;
+    }
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
 </script>
