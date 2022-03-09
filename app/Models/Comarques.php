8aefbec1c7fdb0d72ce5cd12d06d2f00242384a3
Raul Lendines <raullendines@gmail.com>
2022-03-09 10:11:38 +0100

    Comarques i provincies

---
+++ app/Models/Comarques.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comarques extends Model
+{
+    use HasFactory;
+
+    public $table = 'comarques';
+    public $primaryKey = 'id'; //Por defecto es id
+    public $incrementing = true; //Por defecto es true
+    public $timestamps = false;
+
+    public function Provincies()
+    {
+        return $this->belongsTo(Provincies::class, 'provincies_id');
+    }
+}
---
+++ app/Models/Provincies.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Provincies extends Model
+{
+    use HasFactory;
+
+    public $table = 'provincies';
+    public $primaryKey = 'id'; //Por defecto es id
+    public $incrementing = true; //Por defecto es true
+    public $timestamps = false;
+
+    public function Comarques()
+    {
+        return $this->hasMany(Comarques::class, 'provincies_id');
+    }
+}
