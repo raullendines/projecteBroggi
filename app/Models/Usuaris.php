4f7906bc06bf6d95cda25cdea39ff674d85f724c
Raul Lendines <raullendines@gmail.com>
2022-03-09 10:08:04 +0100

    Perfils i usuaris

---
+++ app/Models/Usuaris.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuaris extends Model
+{
+    use HasFactory;
+
+    public $table = 'usuaris';
+    public $primaryKey = 'id'; //Por defecto es id
+    public $incrementing = true; //Por defecto es true
+    public $timestamps = false;
+
+    public function Perfils()
+    {
+        return $this->belongsTo(Perfils::class, 'perfils_id');
+    }
+}
