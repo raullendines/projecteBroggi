e1d7da9a51a589e09bf3887d6d767d5f721a5307
Raul Lendines <raullendines@gmail.com>
2022-03-09 10:22:04 +0100

    MunicipisAgencies

--- app/Models/Agencies.php
+++ app/Models/Agencies.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Municipis extends Model
+class Agencies extends Model
 {
     use HasFactory;
 
-    public $table = 'municipis';
+    public $table = 'agencies';
     public $primaryKey = 'id'; //Por defecto es id
     public $incrementing = true; //Por defecto es true
     public $timestamps = false;
 
-    public function Comarques()
+    public function Municipis()
     {
-        return $this->belongsTo(Comarques::class, 'comarques_id');
-    }
-    
-    public function Agencies()
-    {
-        return $this->hasMany(Agencies::class, 'municipis_id');
+        return $this->belongsTo(Municipis::class, 'municipis_id');
     }
 }
