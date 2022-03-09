44e539328fd0ffbb06205a6131e65df2a2808f99
Raul Lendines <raullendines@gmail.com>
2022-03-09 10:14:51 +0100

    Municipis

--- app/Models/Municipis.php
+++ app/Models/Municipis.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comarques extends Model
+class Municipis extends Model
 {
     use HasFactory;
 
-    public $table = 'comarques';
+    public $table = 'municipis';
     public $primaryKey = 'id'; //Por defecto es id
     public $incrementing = true; //Por defecto es true
     public $timestamps = false;
 
-    public function Provincies()
+    public function Comarques()
     {
-        return $this->belongsTo(Provincies::class, 'provincies_id');
-    }
-
-    public function Municipis()
-    {
-        return $this->hasMany(Municipis::class, 'comarques_id');
+        return $this->belongsTo(Comarques::class, 'comarques_id');
     }
 }
