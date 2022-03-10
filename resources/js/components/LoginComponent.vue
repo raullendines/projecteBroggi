ba8ca0792b35c9580e4281c55de5427bdb571987
Raul Lendines <raullendines@gmail.com>
2022-03-10 13:53:51 +0100

    Form

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container border border-dark rounded bg-light">
-        <div class="row justify-content-center m-4">
-            <div class="col-md-4">
-                <img src="" alt=""/>
+        <div class="row d-flex align-items-center">
+            <div class="col-xl-5 col-12">
+                <img :src="'./img/broggi_bg.png'" class="mx-auto d-block"/>
             </div>
-                <div class="col-md-6 container">
+                <div class="col-xl-6 container mb-3">
                     <form needs-validation>
                         <div class="row">
                         <h1>Login</h1>
@@ -12,20 +12,18 @@
                                 <div class="form-floating mb-3">
                                     <input type="email" class="form-control" id="email" placeholder="Adreça electrònica" oninput="validation()"/>
                                     <label for="email">Adreça electrònica</label>
-                                    <div id="feedback-email"></div>
-                                </div>
+<!--                                     <div id="feedback-email"></div>-->
+                                 </div>
                             </div>
-                        </div>
-                        <div class="row align-items-center">
-                            <div class="col-10">
-                                <div class="form-floating">
+                            <div class="col-12">
+                                <!-- Se me esta quitando el label ayudaaaa -->
+                                <div class="input-group form-floating">
                                     <input :type="tipo" class="form-control" id="password" placeholder="Contrassenya" oninput="validation()"/>
                                     <label for="password">Contrassenya</label>
-                                    <div id="feedback-password"></div>
+<!--                                     <div id="feedback-password"></div>-->  
+                                   <button @click="viewPassword" class="btn input-group-text" type="button" style="background-color: #00b0c8" > <i class="fa fa-eye"></i> </button>
+
                                 </div>
-                            </div>
-                            <div class="col-2">
-                                <button @click="viewPassword" class="btn rounded-pill" type="button" style="background-color: #00b0c8" > <i class="fa fa-eye"></i> </button>
                             </div>
                             <div class="d-grid gap-2 col-12 mt-3">
                                 <button class="btn rounded-pill" type="button" style="background-color: #00b0c8">
