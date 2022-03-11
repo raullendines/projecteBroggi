722a7eb6ed9276cedb23b95426eaf5c73347f7d8
Pau Monedero Roigé <pmonederor2021@cepnet.net>
2022-03-11 10:19:16 +0100

    Fixed all problems and finished

--- public/js/detectDark.js
+++ public/js/detectDark.js
@@ -2,7 +2,7 @@ let favicon = document.getElementById('favicon');
 
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    favicon.href = "{{ asset('images/logoCascClar.png') }}";
+    favicon.href = "images/logoCascClar.png";
 }else{
-    favicon.href = "{{ asset('images/logoCascClar.png') }}";
+    favicon.href = "images/logoCascNegre.png";
 }
